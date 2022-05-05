51e4dd24bd60f152f9072d56709986f398d69ed6
Vinícius Neto <viniciusbsneto@gmail.com>
2022-05-05 01:39:55 -0300

    feat: add metamask connected app example

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
-import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { useMetamask } from './hooks/use-metamask'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { connectToMetamask, ethereumWalletAddress } = useMetamask();
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Connect to Metamask example!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
+          {ethereumWalletAddress ? <span>Address: {ethereumWalletAddress}</span> : <button type="button" onClick={() => connectToMetamask()}>
+            Connect
+          </button>}
         </p>
       </header>
     </div>
